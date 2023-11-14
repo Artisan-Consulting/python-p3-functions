0f8601613c1a2ab5004e82a8295955ea9f89f140
Artisan <william.kwan@artisanconsultin.nz>
2023-11-14 12:27:08 +1100

    Python functions added

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,36 @@
 #!/usr/bin/env python3
 
+
 def greet_programmer():
+    print ("Hello, programmer!")
     pass
+
 
 def greet(name):
+    print("Hello, " + name + "!")
     pass
+
 
 def greet_with_default(name="programmer"):
+    #print("Hello, " + name + "!", end="\n")
+    print("Hello, {}!".format(name))
     pass
+
 
 def add(num1, num2):
+    return num1 + num2
     pass
 
-def halve(number):
+
+def halve(number=100):
+    return number / 2
     pass
+
+def halve_int(number=100):
+    return int(number / 2)
+    pass
+
+def halve_float(number=100):
+    return float(number / 2)
+    pass    
+
